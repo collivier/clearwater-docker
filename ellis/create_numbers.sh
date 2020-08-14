ad3436358cbf603e0e155c42c3d16ba7ba5ab7ef
Cédric Ollivier <cedric.ollivier@orange.com>
2020-08-14 10:56:54 +0200

    Increase sleep before creating numbers in DB
    Signed-off-by: Cédric Ollivier <cedric.ollivier@orange.com>

--- ellis/create_numbers.sh
+++ ellis/create_numbers.sh
@@ -6,6 +6,6 @@ do
 done
 
 # Sometimes it takes a little whilst before it is possible to connect to mysql
-sleep 2
+sleep 10
 
 usr/share/clearwater/ellis/env/bin/python /usr/share/clearwater/ellis/src/metaswitch/ellis/tools/create_numbers.py --start 6505550000 --count 1000
